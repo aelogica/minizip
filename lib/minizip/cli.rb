1e332bd30ac7462635bea1fbac80d9433047b5e0
Eumir Gaspar <eumir@aelogica.com>
2011-08-15 09:56:14 +0800

    added files command to cli

--- lib/minizip/cli.rb
+++ lib/minizip/cli.rb
@@ -3,7 +3,13 @@ require 'minizip/zip'
 
 module Minizip
   class CLI < Thor
-    desc "dir", "dir zip_name (automatically appends .zip) directory_to_zip"
+
+    desc "files", "files zip_name (add .zip if using windows) files_to_zip"
+    def files(zip_name, dir_name)
+      Minizip::Zip.zip_files(zip_name, dir_name)
+    end
+
+    desc "dir", "dir zip_name (autoiaticilly appends .zip) directory_to_zip"
     def dir(zip_name, dir_name)
       Minizip::Zip.zip_dir(zip_name, dir_name)
     end
