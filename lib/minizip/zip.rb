2d9a256aa7d82f6997bf27b095957f8cb44b5590
Radamanthus Batnag <rad@batnag.org>
2011-12-22 12:54:49 +0800

    Added support for Linux

--- lib/minizip/zip.rb
+++ lib/minizip/zip.rb
@@ -2,28 +2,22 @@ require 'rbconfig'
 
 module Minizip
   USING_WINDOWS = (::RbConfig::CONFIG['host_os'] =~/mswin|mingw/)
-  USING_OSX = (::RbConfig::CONFIG['host_os'] =~ /darwin/)
 
   class Zip
     def self.zip_files(zip_name, *files)
-      if USING_OSX
-        system "zip #{zip_name} #{files.join(' ')}"
-      elsif USING_WINDOWS
+      if USING_WINDOWS
         system "7za a #{zip_name} #{files.join(' ')}"
       else
-        raise "Unsupported platform"
+        system "zip #{zip_name} #{files.join(' ')}"
       end
     end
 
     def self.zip_dir(zip_name, directory)
       if File.exists?(directory)
-        if USING_OSX
-          puts "ZIPPING #{directory} to #{zip_name}.zip"
-          system "zip -r #{zip_name} #{directory}"
-        elsif USING_WINDOWS
+        if USING_WINDOWS
           system "7za a #{zip_name} #{directory}"
         else
-          raise "Unsupported platform"
+          system "zip -r #{zip_name} #{directory}"
         end
       else
         puts "#{directory} doesn't exist in #{Dir.pwd}"
@@ -34,20 +28,18 @@ module Minizip
       if File.exists?(zip_name)
         Dir.mkdir(directory) if directory && !File.exists?(directory)
 
-        if USING_OSX
-          if directory
-            system "unzip #{zip_name} -d #{directory}"
-          else
-            system "unzip #{zip_name}"
-          end
-        elsif USING_WINDOWS
+        if USING_WINDOWS
           if directory
             system "7za x #{zip_name} -o#{directory}"
           else
             system "7za x #{zip_name}"
           end
         else
-          raise "Unsupported platform"
+          if directory
+            system "unzip #{zip_name} -d #{directory}"
+          else
+            system "unzip #{zip_name}"
+          end
         end
       else
         puts "#{zip_name} doesn't exist in #{Dir.pwd}"
